e28d2840f5dc7705dbcbbe0e782273c99738ebbb
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-11 14:25:19 +0530

    feat: Add Hello world

---
+++ src/main/kotlin/com/haroldadmin/kshitijchauhan/hellokotlin/Personalised-Hello.kt
@@ -0,0 +1,6 @@
+// Run this example with command line arguments
+// java -jar Personalised-Hello.jar <Your Name>
+fun main(args: Array<String>): Unit {
+    val name = args[0]
+    println("Hello, ${name}!")
+}
